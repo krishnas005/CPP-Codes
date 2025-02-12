b1f6e8d7d0eb9ba42c5144f01a41987ebd63c35b
krishnas05 <kanavkrishna05@gmail.com>
2025-02-12 10:36:45 +0530

    add

--- Segment_Tree.cpp
+++ Segment_Tree.cpp
@@ -1,19 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 struct segmentTree {
     int n;
     vector<int> tree;
+
     void init(int _n) {
         this->n = _n;
         tree.resize(4 * n, 0);
     }
+
     int comb(int a, int b) {
-        // range sum
+        // Range sum operation
         return a + b;
     }
+
     void build(int start, int end, int currIdx, vector<int>& arr) {
-        if(start == end) {
+        if (start == end) {
             tree[currIdx] = arr[start];
             return;
         }
@@ -22,51 +25,53 @@ struct segmentTree {
         build(mid + 1, end, 2 * currIdx + 2, arr);
         tree[currIdx] = comb(tree[2 * currIdx + 1], tree[2 * currIdx + 2]);
     }
+
     int query(int start, int end, int l, int r, int currIdx) {
-        if(r < start || l > end) {
-            return 0;
-        }
-        if(l <= start && r >= end) {
-            return tree[currIdx];
-        }
+        if (r < start || l > end) return 0;  // No overlap
+        if (l <= start && r >= end) return tree[currIdx];  // Complete overlap
         int mid = (start + end) / 2;
         int q1 = query(start, mid, l, r, 2 * currIdx + 1);
         int q2 = query(mid + 1, end, l, r, 2 * currIdx + 2);
         return comb(q1, q2);
     }
+
     void update(int start, int end, int node, int index, int val) {
-        if(start == end) {
-            tree[node] = val;
+        if (start == end) {
+            tree[node] = val; 
             return;
         }
         int mid = (start + end) / 2;
-        if(index <= mid) {
+        if (index <= mid) {
             update(start, mid, 2 * node + 1, index, val);
         } else {
             update(mid + 1, end, 2 * node + 2, index, val);
         }
         tree[node] = comb(tree[2 * node + 1], tree[2 * node + 2]);
-        return;
     }
-    // can be used when we are asked to remove the kth element from the array after the operation, hence we will be marking 0 to removed elements and then we can find the kth element in the array by adding, using this function though segment tree
+
+    // Finds the k-th element in the segment tree
     int walk(int start, int end, int node, int sum, int wantSum) {
-        if(start == end) return start;
+        if (start == end) return start;
         int mid = (start + end) / 2;
-        if(sum + tree[2 * node + 1] >= wantSum) {
+        if (sum + tree[2 * node + 1] >= wantSum) {
             return walk(start, mid, 2 * node + 1, sum, wantSum);
         } else {
             return walk(mid + 1, end, 2 * node + 2, sum + tree[2 * node + 1], wantSum);
         }
     }
+
     void build(vector<int>& arr) {
         build(0, n - 1, 0, arr);
     }
+
     int query(int l, int r) {
         return query(0, n - 1, l, r, 0);
     }
+
     void update(int index, int val) {
         update(0, n - 1, 0, index, val);
     }
+
     int walk(int wantSum) {
         return walk(0, n - 1, 0, 0, wantSum);
     }
@@ -77,8 +82,12 @@ int main() {
     st.init(10);
     vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     st.build(arr);
-    cout << st.query(0, 9) << endl;
+    
+    cout << "Sum from index 0 to 9: " << st.query(0, 9) << endl;  // Output: 55
+    
     st.update(2, -10);
-    cout << st.query(0, 9) << endl;
+    
+    cout << "Sum from index 0 to 9 after update: " << st.query(0, 9) << endl;  // Output: 42
+
     return 0;
 }
